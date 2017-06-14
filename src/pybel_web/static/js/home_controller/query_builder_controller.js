6755f94ba66907f433ccac96ddd4afe0b243135c
ddomingofernandez <danieldomingofernandez@hotmail.com>
2017-06-14 10:51:11 +0200

    Added new style for function pipeline graph.css shadow added to nodes

--- src/pybel_web/static/js/home_controller/query_builder_controller.js
+++ src/pybel_web/static/js/home_controller/query_builder_controller.js
@@ -204,126 +204,66 @@ $(document).ready(function () {
         }
     });
 
-    // Filter dynamic selection
-
-    var wrapper = $(".multi-input-fields"); //Fields wrapper
-
-    var x = 0; //Initial inputField index
-
-    function add_input() {
-
-        $(wrapper).append('<div class="form-group"" style="margin:auto;">' +
-            '<input type= "text" name="pipeline[]" placeholder="Please enter the PyBEL function that will be applied to the query" id="pipeline-number-' + x + '" class="form-control inputCounter" required="">' +
-            '<i class="fa fa-minus-square fa-2x remove-field"></i>' +
-            '</div>'
-        );
-
-        $('#pipeline-number-' + x).autocomplete({
-            source: function (request, response) {
-                $.ajax({
-                    url: "/api/pipeline/suggestion/",
-                    dataType: "json",
-                    data: {
-                        term: request.term
-                    },
-                    success: function (data) {
-                        response(data);
-                    }
-                });
-            }, minLength: 2
-
-        });
-
-        x++; //text box increment
-
-    }
-
-    add_input();
-
-    $(wrapper).on("click", ".add-field", function createNewField(e) { //on add input button click
-            e.preventDefault();
-            add_input()
-        }
-    );
-
-
-    $(wrapper).on("click", ".remove-field", function (e) { //user click on remove text
-        e.preventDefault();
-
-        $(this).parent('div').remove();
-
-        var input_counter = $(".inputCounter");
-
-
-        // If you delete the last input box, set x to zero
-        if (input_counter.length == 0) {
-            x = 0
-        }
-        // else (there are still boxes) update numbers
-        else {
-            input_counter.each(function (elm) {
-                $(this).attr({
-                    "name": "pipeline_number" + (elm)
-                });
+    // Autocompletion in the first input
+    $('#pipeline').autocomplete({
+        source: function (request, response) {
+            $.ajax({
+                url: "/api/pipeline/suggestion/",
+                dataType: "json",
+                data: {
+                    term: request.term
+                },
+                success: function (data) {
+                    response(data);
+                }
             });
-
-            $(".TypeSelector").each(function (elm) {
-                $(this).attr({
-                    "name": "selection-field_" + (elm)
-                });
-                x = elm + 1;
-            });
-        }
-
-        //Update automatically and deselect all selected options (otherwise you'd need to wait until the user makes a selection)
-        $('option[disabled]').prop('disabled', false);
-
-        $('select').each(function () {
-            $('select').not(this).find('option[value="' + this.value + '"]').prop('disabled', true);
-        });
+        }, minLength: 2
     });
 
-    // Filter dynamic selection
-    //
-    // var queryForm = $('#query-form');
-    //
-    // queryForm.submit(function () {
-    //
-    //     /**
-    //      * Creates the parameter object with selection that are not empty
-    //      */
-    //
-    //         // params is the pybel.Query json representation and seeding represents the seed data/type object
-    //     var params = {"seeds": []}, seeding = {};
-    //
-    //     // var networkBoolean = addCheckboxParameter(params, "network_ids");
-    //
-    //     // Dont submit the form if there are no Networks selected
-    //     if (networkBoolean === false) {
-    //         return false
-    //     }
-    //     //
-    //     // addQueryParameters(seeding, "pmids", $("#pubmed_selection").select2("data"), "text");
-    //     // addQueryParameters(seeding, "authors", $("#author_selection").select2("data"), "text");
-    //     // addQueryParameters(seeding, "nodes", $("#node_selection").select2("data"), "id");
-    //     // addPipelineParameters(params, "pipeline");
-    //     //
-    //     // // TODO: NOTICE THAT PROVENANCE SEED TYPE IS NOW DIVIDED INTO pubmed and author
-    //     // $.each(seeding, function (key, value) {
-    //     //     if (key === "pmids") {
-    //     //         params["seeds"].push({type: "pubmed", data: value});
-    //     //     }
-    //     //     else if (key == "authors") {
-    //     //         params["seeds"].push({type: "author", data: value});
-    //     //     }
-    //     //     else if (key === "nodes") {
-    //     //         params["seeds"].push({type: $('input[name=seed_method]:checked').val(), data: value});
-    //     //     }
-    //     // });
-    //
-    //     return true;
-    // });
+
 });
+
+$(function () {
+    // Remove button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="remove"]',
+        function (e) {
+            e.preventDefault();
+            $(this).closest('.form-inline').remove();
+        }
+    );
+    // Add button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="add"]',
+        function (e) {
+            e.preventDefault();
+            var container = $(this).closest('[data-role="dynamic-fields"]');
+            new_field_group = container.children().filter('.form-inline:first-child').clone();
+            new_field_group.find('input').each(function () {
+                $(this).val(''); // Empty current value and start autocompletion
+                $(this).autocomplete({
+                    source: function (request, response) {
+                        $.ajax({
+                            url: "/api/pipeline/suggestion/",
+                            dataType: "json",
+                            data: {
+                                term: request.term
+                            },
+                            success: function (data) {
+                                response(data);
+                            }
+                        });
+                    }, minLength: 2
+                });
+            });
+            container.append(new_field_group);
+        }
+    );
+});
+
+
 
 
 
