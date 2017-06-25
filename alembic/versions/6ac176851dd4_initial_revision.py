b7de13f91ed8c21bc0ed93b32ab96f03d62850d3
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-06-25 18:31:50 +0200

    Add alembic

---
+++ alembic/versions/6ac176851dd4_initial_revision.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""initial revision
+
+Revision ID: 6ac176851dd4
+Revises: 
+Create Date: 2017-05-22 13:12:50.405757
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6ac176851dd4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
