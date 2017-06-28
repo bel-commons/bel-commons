672edf2a6e16c8ed307250c890be19b236703ed8
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-06-28 23:02:41 +0200

    Added super badass project view for normal users
    Also enable admin panel for all users

--- src/pybel_web/admin_service.py
+++ src/pybel_web/admin_service.py
@@ -5,11 +5,14 @@ import logging
 from flask import redirect, url_for, request
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView as ModelViewBase
+from flask_admin.contrib.sqla.ajax import QueryAjaxModelLoader
+from flask_admin.model.ajax import DEFAULT_PAGE_SIZE
 from flask_security import current_user
+from sqlalchemy import or_
 
 from pybel.manager.models import Network, Namespace, Annotation
 from .application import get_manager
-from .models import Report, Experiment, Role, User, Query, Assembly
+from .models import Report, Experiment, Role, User, Query, Assembly, Project
 
 log = logging.getLogger(__name__)
 
@@ -53,6 +56,34 @@ def build_admin_service(app):
     admin.add_view(ModelView(Experiment, manager.session))
     admin.add_view(ModelView(Query, manager.session))
     admin.add_view(ModelView(Assembly, manager.session))
+
+    class NetworkAjaxModelLoader(QueryAjaxModelLoader):
+        def __init__(self):
+            super(NetworkAjaxModelLoader, self).__init__('networks', manager.session, Network, fields=[Network.name])
+
+        def get_list(self, term, offset=0, limit=DEFAULT_PAGE_SIZE):
+            """Overrides get_list to be lazy and tricky about only getting current user's networks"""
+            query = self.session.query(self.model)
+
+            filters = (field.ilike(u'%%%s%%' % term) for field in self._cached_fields)
+            query = query.filter(or_(*filters))
+
+            query = query.filter(Network.id.in_(network.id for network in current_user.get_owned_networks()))
+
+            return query.offset(offset).limit(limit).all()
+
+    class ProjectView(ModelViewBase):
+        """Special view to allow users of given projects to manage them"""
+
+        def get_query(self):
+            return super(ProjectView, self).get_query().filter(
+                Project.id.in_(project.id for project in current_user.projects))
+
+        form_ajax_refs = {
+            'networks': NetworkAjaxModelLoader()
+        }
+
+    admin.add_view(ProjectView(Project, manager.session))
 
     log.info('Added admin service for %s', app)
 
