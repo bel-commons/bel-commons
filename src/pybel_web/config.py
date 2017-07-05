2f731def2fe9c942ddc56b075b059e63ce87c103
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-07-05 16:00:54 +0200

    Update default config

--- src/pybel_web/config.py
+++ src/pybel_web/config.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from .constants import CHARLIE_EMAIL
+
 
 class Config:
     """This is the default configuration to be used in a development environment. It assumes you have:
@@ -8,19 +10,28 @@ class Config:
     - RabbitMQ or another message broker supporting the AMQP protocol running on localhost
     """
     #: The Flask app secret key. CHANGE THIS
-    SECRET_KEY = 'pybel_default_key'
+    SECRET_KEY = 'pybel_not_default_key1234567890"'
+    SERVER_NAME = 'pybel.scai.fraunhofer.de'
     DEBUG = False
     TESTING = False
 
     CELERY_BROKER_URL = 'amqp://localhost'
 
     SECURITY_REGISTERABLE = True
-    SECURITY_CONFIRMABLE = False
-    SECURITY_SEND_REGISTER_EMAIL = False
+    SECURITY_CONFIRMABLE = True
+    SECURITY_SEND_REGISTER_EMAIL = True
+    SECURITY_RECOVERABLE = True
     #: What hash algorithm should we use for passwords
     SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
     #: What salt should we use to hash passwords? DEFINITELY CHANGE THIS
-    SECURITY_PASSWORD_SALT = 'pybel_default_salt'
+    SECURITY_PASSWORD_SALT = 'pybel_not_default_salt1234567890'
+    SECURITY_EMAIL_SENDER = 'PyBEL Web <pybel@scai.fraunhofer.de>"'
+
+    MAIL_SERVER = 'localhost'
+    MAIL_PORT = 25
+    MAIL_USERNAME = 'pybel@scai.fraunhofer.de'
+    # Automatically sends mail from this account
+    MAIL_DEFAULT_SENDER = 'PyBEL Web <pybel@scai.fraunhofer.de>'
 
     #: A connection string for the PyBEL cache
     PYBEL_CONNECTION = None
@@ -33,3 +44,6 @@ class Config:
     PYBEL_DS_EAGER = False
     #: Should networks be saved to the edge store?
     PYBEL_USE_EDGE_STORE = False
+
+    PYBEL_WEB_STARTUP_NOTIFY = CHARLIE_EMAIL
+    PYBEL_WEB_REPORT_RECIPIENT = CHARLIE_EMAIL
