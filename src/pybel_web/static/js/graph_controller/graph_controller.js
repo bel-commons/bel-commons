c7edababd49903fe348c1c8a329cc2ad966c02ae
jmarinllao <josepmarinllao@gmail.com>
2017-07-06 11:35:53 +0200

    Added isolating node option.

--- src/pybel_web/static/js/graph_controller/graph_controller.js
+++ src/pybel_web/static/js/graph_controller/graph_controller.js
@@ -620,6 +620,18 @@ function initD3Force(graph, tree) {
                 });
             }
         }
+         {
+            title: "Create a query isolating this node.",
+            action: function (elm, node, i) {
+                $.ajax({
+                    url: "/api/query/" + window.query + "/isolated_node/" + node.id,
+                    dataType: "json"
+                }).done(function (response) {
+                    updateQueryResponse(response, tree);
+                });
+            }
+        }
+
     ];
 
     // Definition of context menu for nodes
