3ea6683fc1a20c2582d4e1d645458dd1cf528278
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-07-29 18:53:35 +0200

    Removed imports from init

--- src/pybel_web/__init__.py
+++ src/pybel_web/__init__.py
@@ -9,20 +9,6 @@ These endpoints should expose data as JSON, and not rely on templates, since the
 packages and locations
 """
 
-from __future__ import print_function
-
-from .admin_service import build_admin_service
-from .analysis_service import analysis_blueprint
-from .application import create_application
-from .constants import *
-from .curation_service import curation_blueprint
-from .database_service import api_blueprint
-from .main_service import build_main_service
-from .models import Role, User
-from .parser_async_service import parser_async_blueprint
-from .parser_endpoint import build_parser_service
-from .upload_service import upload_blueprint
-
 __version__ = '0.1.1'
 
 __title__ = 'pybel_web'
