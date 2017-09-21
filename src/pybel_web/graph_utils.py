77b9886cb82be2d5fdac20fef187a0748ebfb0f5
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-09-21 12:14:23 +0200

    Add unit tests written with Alex

--- src/pybel_web/graph_utils.py
+++ src/pybel_web/graph_utils.py
@@ -3,25 +3,39 @@
 from pybel.constants import ANNOTATIONS
 
 
+def _annotation_dict_filter_helper(data, annotations, values):
+    """Returns if any of the annotations and values match up
+
+    :param dict data:
+    :param list[str] annotations: A list of the annotations to search
+    :param list[str] values: A list of values to match against any annotations
+    :rtype: bool
+    """
+    if ANNOTATIONS not in data:
+        return False
+
+    data_annotations = data[ANNOTATIONS]
+
+    for annotation in annotations:
+        if annotation not in data_annotations:
+            continue
+
+        if any(value in data_annotations[annotation] for value in values):
+            return True
+
+    return False
+
+
 def build_annotation_search_filter(annotations, values):
     """Builds an annotation search filter for Mozg
 
     :param list[str] annotations: A list of the annotations to search
     :param list[str] values: A list of values to match against any annotations
-    :rtype: bool
+    :return: Edge filter function
     """
 
     def annotation_dict_filter(graph, u, v, k, d):
         """Returns if any of the annotations and values match up"""
-        if ANNOTATIONS not in d:
-            return False
-
-        for annotation in annotations:
-            if annotation not in d[ANNOTATIONS]:
-                continue
-            if any(value in d[ANNOTATIONS][annotation] for value in values):
-                return True
-
-        return False
+        return _annotation_dict_filter_helper(d, annotations, values)
 
     return annotation_dict_filter
