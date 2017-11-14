f37e3ef88b47bf3b6e5968276e5029dd2fad0690
Charles Tapley Hoyt <cthoyt@gmail.com>
2017-11-14 23:08:59 +0100

    Remove unused constants

--- src/pybel_web/constants.py
+++ src/pybel_web/constants.py
@@ -3,28 +3,12 @@
 import os
 from logging import getLogger
 
-import pybel_tools.constants
-from pybel.constants import PYBEL_DATA_DIR, PYBEL_LOG_DIR
+from pybel.constants import PYBEL_DATA_DIR
 
 PYBEL_WEB_VERSION = '0.2.1-dev'
 
-PYBEL_CACHE_CONNECTION = 'pybel_cache_connection'
-PYBEL_DEFINITION_MANAGER = 'pybel_definition_manager'
-PYBEL_METADATA_PARSER = 'pybel_metadata_parser'
-PYBEL_GRAPH_MANAGER = 'pybel_graph_manager'
-SECRET_KEY = 'SECRET_KEY'
-
-PYBEL_GITHUB_CLIENT_ID = 'PYBEL_GITHUB_CLIENT_ID'
-PYBEL_GITHUB_CLIENT_SECRET = 'PYBEL_GITHUB_CLIENT_SECRET'
-PYBEL_WEB_PASSWORD_SALT = 'PYBEL_WEB_PASSWORD_SALT'
-
 integrity_message = "A graph with the same name ({}) and version ({}) already exists. If there have been changes since the last version, try bumping the version number."
 reporting_log = getLogger('pybelreporting')
-
-DEFAULT_SERVICE_URL = 'http://pybel.scai.fraunhofer.de'
-
-DICTIONARY_SERVICE = 'dictionary_service'
-DEFAULT_TITLE = 'Biological Network Explorer'
 
 NETWORK_ID = 'network_id'
 SOURCE_NODE = 'source'
