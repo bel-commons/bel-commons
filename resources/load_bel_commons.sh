a00019a70d4eebec820848a231f56f650213d106
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-24 17:21:41 +0100

    Put loaders in main shell script

--- resources/load_bel_commons.sh
+++ resources/load_bel_commons.sh
@@ -2,3 +2,7 @@
 
 # This script loads all of the sample resources for BEL Commons.
 # Assumes: the environment variable BMS_BASE is set.
+
+python3 load_omics.py
+python3 load_networks.py
+python3 load_experiments.py
