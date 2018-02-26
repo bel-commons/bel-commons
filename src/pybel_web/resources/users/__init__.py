7e40ac2bfecedf4975dfc8c5c47204fd6392eecc
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-02-26 10:25:28 +0100

    Add missing users init folder

---
+++ src/pybel_web/resources/users/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""Stores default user information"""
+
+import logging
+import os
+
+log = logging.getLogger(__name__)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+default_users_path = os.path.join(dir_path, 'default_users.json')
