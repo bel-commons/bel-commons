32befe38f40f89dcff391d4822ece4b4077686d2
Charles Tapley Hoyt <cthoyt@gmail.com>
2018-04-27 11:04:26 +0200

    Update reporting page

--- src/pybel_web/views/reporting.py
+++ src/pybel_web/views/reporting.py
@@ -4,11 +4,12 @@
 
 import datetime
 import logging
+from collections import Counter
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_security import roles_required
 
-from ..models import Report
+from ..models import EdgeComment, EdgeVote, Experiment, Query, Report
 from ..proxies import manager
 
 log = logging.getLogger(__name__)
@@ -16,20 +17,57 @@ log = logging.getLogger(__name__)
 reporting_blueprint = Blueprint('reporting', __name__, url_prefix='/reporting')
 
 
+def _get_timeseries_from_models(models, y_axis_name, x_axis_name='x'):
+    date_counter = Counter(created.strftime('%Y-%m-%d') for created, in models)
+
+    if not date_counter:
+        return
+
+    x_list, y_list = zip(*date_counter.most_common())
+
+    return [
+        [x_axis_name] + list(x_list),
+        [y_axis_name] + list(y_list),
+    ]
+
+
+def _get_timeseries(model, interval, name):
+    models = manager.session.query(model.created)
+    models = models.filter(model.created > interval)
+    return _get_timeseries_from_models(models, name)
+
+
+def _get_vote_timeseries(interval, name):
+    models = manager.session.query(EdgeVote.changed)
+    models = models.filter(EdgeVote.changed > interval)
+    return _get_timeseries_from_models(models, name)
+
+
 @reporting_blueprint.route('/')
 @roles_required('admin')
 def view_report():
     """This view gives an overview of the user actions in the last 7 days"""
-    interval = datetime.datetime.now() - datetime.timedelta(days=7)
+    days = request.args.get('days', default=7, type=int)
+    interval = datetime.datetime.now() - datetime.timedelta(days=days)
 
-    reports = manager.session.query(Report).filter(Report.created > interval)
+    network_data = _get_timeseries(Report, interval, 'Network Uploads')
+    query_data = _get_timeseries(Query, interval, 'Queries')
+    experiment_data = _get_timeseries(Experiment, interval, 'Experiments')
+    vote_data = _get_vote_timeseries(interval, 'Votes')
+    comment_data = _get_timeseries(EdgeComment, interval, 'Comments')
 
-    date_report = {
-        report.created.strftime('%Y%m%d'): report
-        for report in reports
+    charts = {
+        'network-chart': network_data,
+        'query-chart': query_data,
+        'experiment-chart': experiment_data,
+        'vote-chart': vote_data,
+        'comment-chart': comment_data
     }
 
-    return render_template('reporting/report.html', date_report=date_report)
+    return render_template(
+        'reporting/report.html',
+        charts=charts
+    )
 
 
 @reporting_blueprint.route('/network', methods=['GET'])
